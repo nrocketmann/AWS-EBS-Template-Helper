17ec3dd8f5e3fa42be2ab008955dbf53c63079a1
nrocketmann <nameerhirschkind@gmail.com>
2016-05-24 15:58:02 -0700

    Add files via upload

---
+++ application.py
@@ -0,0 +1,2 @@
+"""This is the final application doc. NOT TO BE EDITED UNTIL THE HACKATHON. Static files go in the /static/ folder.
+Static files are .js functions or CSS markings that you don't want to put in the template. We don't have to use it, but I set up a static files bin. Templates all go in the templates folder. Name them properly and descriptively. Requirements.txt has our requirements to be compatible with aws ebs"""
